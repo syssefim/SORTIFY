d78759832cc2e03a2103d7ca81e784ad43e46717
George Tsetsegmaa <macdev@macdev-2.local>
2026-02-24 17:49:37 -0800

    Add logging and track the number of hits on the server

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
 func main() {
-	const port = 8080
+	const filepathRoot = "."
+	const port = "8080"
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+
+	apiCfg := apiConfig{
+		fileServerHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", middlewareLog(apiCfg.middlewareMetricsInc(handler)))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
-		Addr
+		Addr:    ":" + port,
+		Handler: mux,
 	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
